e76154d8aaabfe4adb448a75ed7644e610e18a40
Toni Suominen <toni.suominen@reaktor.com>
2016-03-31 12:55:09 +0300

    Add tmux.conf

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="gitconfig bashrc vimrc vim zshrc"    # list of files/folders to symlink in homedir
+files="gitconfig bashrc vimrc vim zshrc tmux.conf"    # list of files/folders to symlink in homedir
 
 ##########
 
